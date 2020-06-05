fc7205dea3c753a85241f97188b525072c2c5a94
kushalp+1-at-964293474868 <pathakkushal1@gmail.coim>
2020-06-05 21:44:36 +0530

    remove the test cases

--- src/test/java/com/kushal/jwt/jwt/example/repository/DBRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kushal.jwt.jwt.example.repository;
-
-import org.springframework.data.repository.Repository;
-
-public interface DBRepository extends Repository<DBRepository,Integer> {
-}
