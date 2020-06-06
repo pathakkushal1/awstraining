fed87c9843299093db46154e0929b72918060bb7
kushalp+1-at-964293474868 <pathakkushal1@gmail.coim>
2020-06-06 17:53:52 +0530

    fixing the builds

--- src/main/java/com/kushal/jwt/jwt/example/repository/DBRepository.java
+++ src/main/java/com/kushal/jwt/jwt/example/repository/DBRepository.java
@@ -3,12 +3,9 @@ package com.kushal.jwt.jwt.example.repository;
 import com.kushal.jwt.jwt.example.exception.ExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -18,7 +15,6 @@ public class DBRepository {
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
-    ExceptionHandler exceptionHandler;
 
     public List<Map<String, Object>> query(final String query, final Map<String, Object> parameters)  {
             try {
